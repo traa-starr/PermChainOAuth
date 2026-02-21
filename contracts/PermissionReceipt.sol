5da3610832186c55c42a20cb4f85a2460741abf6
traa-starr <tratheboss@gmail.com>
2026-02-20 20:18:42 -0500

    Update PermissionReceipt.sol

--- contracts/PermissionReceipt.sol
+++ contracts/PermissionReceipt.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract PermissionReceipt is ERC721URIStorage {
     error NotGranter();
